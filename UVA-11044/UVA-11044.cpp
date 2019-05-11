decaf9b0ee890f7af775c1ebbbaa01bfb696edf7
Nelson Castro <00043516@uca.edu.sv>
2019-05-11 15:19:08 +0000

    Empty template

---
+++ UVA-11044/UVA-11044.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    return 0;
+}
